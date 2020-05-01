8af396d78378ef8f60d2b4c0d66b16a3cef27b4d
hitu <hitu@192.168.1.2>
2020-05-01 18:15:35 +0530

    Pushing to Github.

---
+++ src/com/hitu/proj1/HelloWorldTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.hitu.proj1;
+
+/**
+ * @author hitu
+ *
+ */
+public class HelloWorldTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Im learning.");
+
+	}
+
+}
+
